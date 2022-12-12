7569b09f9d87942d0df5d8e6de4799bdb5b81c2a
gstp <104087436+graciasitepu@users.noreply.github.com>
2022-12-12 22:27:54 +0700

    Add files via upload

---
+++ Pertemuan5/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbName = "dataMahasiswa";
+    
+    $connection = mysqli_connect("$host", $user, $password, $dbName);
+
+    if(!$connection){
+        die("Koneksi gagal : ".mysqli_connect_error());
+    }
+
+?>
